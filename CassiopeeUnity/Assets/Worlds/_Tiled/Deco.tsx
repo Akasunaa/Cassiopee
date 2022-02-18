9eb5071d37f3c7624c4ad81d461f8f345dd6b1c5
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-02-18 19:33:02 +0100

    Debut du projet

---
+++ CassiopeeUnity/Assets/Worlds/_Tiled/Deco.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Deco" tilewidth="50" tileheight="50" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3" type="/Base/MultiDisplay">
+  <properties>
+   <property name="ImagePool" value=""/>
+   <property name="Materials" value=""/>
+   <property name="Snap" type="bool" value="true"/>
+   <property name="Variable" value=""/>
+   <property name="Y" type="int" value="0"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/multi-display.png"/>
+ </tile>
+ <tile id="4" type="/Base/Texture">
+  <properties>
+   <property name="Materials" value=""/>
+   <property name="Scale" type="float" value="100"/>
+   <property name="Snap" type="bool" value="true"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/texture.png"/>
+ </tile>
+ <tile id="5" type="/Base/Scenery">
+  <properties>
+   <property name="SceneryName" value=""/>
+  </properties>
+  <image width="50" height="50" source="Master Images/scenery.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Anchor" value=""/>
+   <property name="Move" value=""/>
+   <property name="Offset" value=""/>
+   <property name="Rotate" value=""/>
+   <property name="Scale" value=""/>
+   <property name="Snap" type="bool" value="false"/>
+   <property name="Text" value=""/>
+   <property name="Y" type="int" value="0"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/object.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Anchor" value=""/>
+   <property name="Move" value=""/>
+   <property name="Offset" value=""/>
+   <property name="Rotate" value=""/>
+   <property name="Scale" value=""/>
+   <property name="Snap" type="bool" value="false"/>
+   <property name="Text" value=""/>
+   <property name="Y" type="int" value="0"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/object2.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Anchor" value=""/>
+   <property name="Move" value=""/>
+   <property name="Offset" value=""/>
+   <property name="Rotate" value=""/>
+   <property name="Scale" value=""/>
+   <property name="Snap" type="bool" value="false"/>
+   <property name="Text" value=""/>
+   <property name="Y" type="int" value="0"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/object3.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Anchor" value=""/>
+   <property name="Move" value=""/>
+   <property name="Offset" value=""/>
+   <property name="Rotate" value=""/>
+   <property name="Scale" value=""/>
+   <property name="Snap" type="bool" value="false"/>
+   <property name="Text" value=""/>
+   <property name="Y" type="int" value="0"/>
+  </properties>
+  <image width="50" height="50" source="Master Images/object4.png"/>
+ </tile>
+ <tile id="10" type="/Base/GLB">
+  <properties>
+   <property name="Anchor" value=""/>
+   <property name="Move" value=""/>
+   <property name="Offset" value=""/>
+   <property name="Rotate" value=""/>
+   <property name="Scale" value=""/>
+   <property name="Url" value=""/>
+  </properties>
+  <image width="50" height="50" source="Master Images/glb.png"/>
+ </tile>
+</tileset>
